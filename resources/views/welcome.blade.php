9cb55f888498dd9ea48401e9cc4e3523a4e29488
Muhammadirfan08 <irfan020807@gmail.com>
2025-12-03 11:30:57 +0700

    menambahkan model tasks

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,18 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-</head>
-<body>
-    
-    <div class="bg-dark">
-        <div class="container py-3">
-        <div class="h1 text-white">Todo List App</div>
-    </div>
-    </div>
+<!-- @extends('layouts.main') -->
+ @push('head')
+     <title>Todo List App</title>
+ @endpush
 
-</body>
-</html>
+    @section('main-section')
+        <div class="container d-flexmy-5">
+            <div class="h2">All Todos</div>
+            <a href="" class="btn btn-primary">Add New Todo</a>
+        </div>
+    @endsection
